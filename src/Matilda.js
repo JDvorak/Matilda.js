96b630ddc438cfa543e613a30fc277be216ee9ad
John Dvorak <TwelveTwo@Gmail.com>
2013-04-28 11:18:33 -0700

    Converted to Modular Pattern

--- src/Matilda.js
+++ src/Matilda.js
@@ -2,232 +2,270 @@
  * Webscale Statistical Inference
  */
 
-function Model(params) {
+var Matilda = {};
 
-  var topics            = [], 
-      numberOfTopics    = 5,   
-      topicWeights      = [],    
-      documents         = [], 
-      numberOfDocuments = 0, 
-      wordCount         = 0,
-      numberOfWords     = 0,
-      beta              = 0,
-      alpha             = 0,    
-      words             = {};
+Matilda.Model = (function () {
+  var emptyMatrix = function(height, width) {
+    var height = height,
+        width = width || height, 
+        newMatrix = []; 
 
-  this.setNumberOfTopics = function(K) {
-    numberOfTopics = K
-    recalculateAlpha();
-    initializeTopics();
-    return this;
-  }; 
+    for (var t1 = 0; t1 < height; t1++) {
+      newMatrix[t1] = [];
+      for (var t2 = 0; t2 < width; t2++) {
+        newMatrix[t1][t2] = 0.0;
+      }
+    }
 
-  this.toString = function() {
-    console.log(topics, topicWeights, documents, words);
+    return newMatrix;
+  }
+
+  var matrixEach = function(matrix, callback, context) {
+    var len = matrix.length;
+    for (var t1 = 0; t1 < len; t1++) {
+      for (var t2 = 0; t2 < len; t2++) {
+        callback.call(context, t1, t2, matrix)
+      }  
+    }
+  }
+
+  var weightedRandom = function(weights, sum) {
+    var sample = sum * Math.random(),
+             i = 0;
+
+    sample -= weights[i];
+
+    while (sample > 0.0) {
+      i++;
+      sample -= weights[i];
+    }
+    return i;
   };
 
-  this.train = function(n, callback, context) {
-    var sum, curWord;
+  function Model(params) {
+    var topics            = [], 
+        numberOfTopics    = 5,   
+        topicWeights      = [],    
+        documents         = [], 
+        numberOfDocuments = 0, 
+        wordCount         = 0,
+        numberOfWords     = 0,
+        beta              = 0,
+        alpha             = 0,    
+        words             = {};
 
-    for (var iterations = 0; iterations < n; iterations++) {
-      documents.forEach(function(currentDoc, i) {
-        for (var w = 0; w < currentDoc.wordCount; w++) {
-          curWord = currentDoc.bagOfWords[w];
+    var recalculateAlpha = function() {
+      beta = numberOfTopics/50
+    }
+    
+    var recalculateBeta = function() {
+      alpha = numberOfWords/200;
+    }
 
-          topics[curWord.isTopic].withWord[curWord.id]--;
-          topics[curWord.isTopic].wordTotal--;
-          
-          sum = 0;
-          
-          for (var t = 0; t < numberOfTopics; t++) {
-            topicWeights[t]  = (beta + currentDoc.topicsCounts[t])  
-            topicWeights[t] *= (alpha + topics[t].withWord[curWord.id]) 
-            topicWeights[t] /= (numberOfWords * beta + topics[t].wordTotal);               
-            sum += topicWeights[t]
-          }
-          curWord.isTopic = weightedRandom(topicWeights, sum)
-
-          topics[curWord.isTopic].withWord[curWord.id]++;
-          topics[curWord.isTopic].wordTotal++;
-        }
-
-        //Normalize Weights, and while we're at it, clear the topicCounts.
-        for (var t = 0; t < numberOfTopics; t++) {
-          topicWeights[t] /= sum;
-          currentDoc.topicsCounts[t] = 0;
-        };
-
-        //Now put them back now.
-        currentDoc.bagOfWords.forEach(function (token) {
-          currentDoc.topicsCounts[token.isTopic] += 1;
-        });
-        
-      });
-
-      nowCallback(callback, context)
+    var initializeTopics = function(){
+      topics = [];
+      topicWeights = [];
+      for (var k = 0; k < numberOfTopics; k++) {
+        topicWeights[k] = (1/numberOfTopics);
+        topics[k] = {
+                    id:       k,
+                    withWord:  {}, 
+                    wordTotal: 0, 
+                    };
+      };
+    for (w in words) {assignRandomly(words[w])};
     };
-    return this;
-  };
 
-  this.addDocument = function(doc, callback, context){
-    if (doc instanceof Array) {
-      if (doc[0] instanceof Array) {
-        for (var subDoc = 0, len = doc.length; subDoc < len; subDoc++) {
-          this.addDocument(doc[subDoc]);
-        }
-      } else {
-        var token,
-            newDoc = {
-                      bagOfWords: [], 
-                      wordCount: 0,
-                      topicsCounts: []
-                     };
-        for (t in topics) {
-          newDoc.topicsCounts[t] = 0;
-        };
+    var assignRandomly = function(word) {
+      var random = Math.floor(Math.random() * numberOfTopics);
+      word.isTopic = random;
+      if (!topics[random].withWord[word.id]) topics[random].withWord[word.id] = 0;
+      topics[random].withWord[word.id]++;
+      topics[random].wordTotal++;
+    };
 
-        for (w in doc) {
-          token = doc[w];
+    var nowCallback = function(callback, context, whatElse) {
+      if (!callback) return;
+      var dataObject = {topics: topics, 
+                        vocab: words,
+                        documents: documents
+                       };
 
-          if (!words[token]) {
-            words[token] = {
-                            id: token,
-                            isTopic: 0, 
-                            total: 0
-                           };
+      callback.call(context, dataObject, whatElse);
+    };
 
-            for (t in topics) {
-              topics[t].withWord[token] = 0;
-            }
+    // / Methods / /    / 
 
-            numberOfWords++;
-            recalculateBeta();
+    this.setNumberOfTopics = function(K) {
+      numberOfTopics = K;
+      recalculateAlpha();
+      initializeTopics();
+      return this;
+    }; 
+
+    this.addDocument = function(doc, callback, context){
+      if (doc instanceof Array) {
+        if (doc[0] instanceof Array) {
+          for (var subDoc = 0, len = doc.length; subDoc < len; subDoc++) {
+            this.addDocument(doc[subDoc]);
+          }
+        } else {
+          var token,
+              newDoc = {
+                        bagOfWords: [], 
+                        wordCount: 0,
+                        topicsCounts: []
+                       };
+          for (t in topics) {
+            newDoc.topicsCounts[t] = 0;
           };
 
-          assignRandomly(words[token]);
-          if (!newDoc.topicsCounts[words[token].isTopic]) newDoc.topicsCounts[words[token].isTopic] = 0;
-          newDoc.topicsCounts[words[token].isTopic]++;
-          newDoc.wordCount++;
-          words[token].total++;
-          wordCount++;
-          newDoc.bagOfWords.push(words[token]);
+          for (w in doc) {
+            token = doc[w];
+
+            if (!words[token]) {
+              words[token] = {
+                              id: token,
+                              isTopic: 0, 
+                              total: 0
+                             };
+
+              for (t in topics) {
+                topics[t].withWord[token] = 0;
+              }
+
+              numberOfWords++;
+              recalculateBeta();
+            };
+
+            assignRandomly(words[token]);
+            if (!newDoc.topicsCounts[words[token].isTopic]) newDoc.topicsCounts[words[token].isTopic] = 0;
+            newDoc.topicsCounts[words[token].isTopic]++;
+            newDoc.wordCount++;
+            words[token].total++;
+            wordCount++;
+            newDoc.bagOfWords.push(words[token]);
+          }
+
+          documents.push(newDoc);
+          numberOfDocuments++;
+
+          nowCallback(callback, context, doc)
         }
+      } else {
+        throw new Error("Pre-process Your Data and Try Again.");
+      };
 
-        documents.push(newDoc);
-        numberOfDocuments++;
-
-        nowCallback(callback, context, doc)
-      }
-    } else {
-      throw new Error("Pre-process Your Data and Try Again.");
-    };
-
-    return this;
-  }
-
-  this.getDocuments = function() {
-    return documents;
-  }
-
-  this.getTopics = function() {
-    return topics;
-  }
-
-  this.getVocabulary = function() {
-    return words;
-  }
-
-  this.topicCorrelations = function(){
-    var correlationMatrix = emptyMatrix(numberOfTopics),
-        meanLogLikelihood = [],
-        logLikelihoodOfDoc = [],
-        normalizer = 1.0 / (numberOfDocuments - 1),
-        standardDeviations = [];
-
-
-    for (var t = 0; t < numberOfTopics; t++) {
-      meanLogLikelihood[t] = 0.0;
+      return this;
     }
 
-    documents.forEach(function(currentDoc, i) {
-      var bagOfWordsLength = currentDoc.bagOfWords.length
+    this.train = function(n, callback, context) {
+      var sum, curWord;
 
-      logLikelihoodOfDoc[i] = [];
+      for (var iterations = 0; iterations < n; iterations++) {
+        documents.forEach(function(currentDoc, i) {
+          for (var w = 0; w < currentDoc.wordCount; w++) {
+            curWord = currentDoc.bagOfWords[w];
+
+            topics[curWord.isTopic].withWord[curWord.id]--;
+            topics[curWord.isTopic].wordTotal--;
+            
+            sum = 0;
+            
+            for (var t = 0; t < numberOfTopics; t++) {
+              topicWeights[t]  = (beta + currentDoc.topicsCounts[t])  
+              topicWeights[t] *= (alpha + topics[t].withWord[curWord.id]) 
+              topicWeights[t] /= (numberOfWords * beta + topics[t].wordTotal);               
+              sum += topicWeights[t]
+            }
+            curWord.isTopic = weightedRandom(topicWeights, sum)
+
+            topics[curWord.isTopic].withWord[curWord.id]++;
+            topics[curWord.isTopic].wordTotal++;
+          }
+
+          for (var t = 0; t < numberOfTopics; t++) {
+            topicWeights[t] /= sum;
+            currentDoc.topicsCounts[t] = 0;
+          };
+
+          currentDoc.bagOfWords.forEach(function (token) {
+            currentDoc.topicsCounts[token.isTopic] += 1;
+          });
+          
+        });
+
+        nowCallback(callback, context)
+      };
+      return this;
+    };
+
+    this.topicCorrelations = function(){
+      var correlationMatrix = emptyMatrix(numberOfTopics),
+          meanLogLikelihood = [],
+          logLikelihoodOfDoc = [],
+          normalizer = 1.0 / (numberOfDocuments - 1),
+          standardDeviations = [];
+
+
       for (var t = 0; t < numberOfTopics; t++) {
-        logLikelihoodOfDoc[i][t] = Math.log((currentDoc.topicsCounts[t] + beta) /
-                                           (bagOfWordsLength + numberOfTopics * beta))
-        meanLogLikelihood[t] += logLikelihoodOfDoc[i][t];
-
+        meanLogLikelihood[t] = 0.0;
       }
-    });
 
-    for (var t = 0; t < numberOfTopics; t++) {
-      meanLogLikelihood[t] /= numberOfDocuments;
-    }
+      documents.forEach(function(currentDoc, i) {
+        var bagOfWordsLength = currentDoc.bagOfWords.length
 
-    for (var d = 0; d < numberOfDocuments; d++) {      
-      matrixEach(correlationMatrix, function(t1, t2){
-        correlationMatrix[t1][t2] +=  (logLikelihoodOfDoc[d][t1] - meanLogLikelihood[t1]) *
-                                      (logLikelihoodOfDoc[d][t2] - meanLogLikelihood[t2])
+        logLikelihoodOfDoc[i] = [];
+        for (var t = 0; t < numberOfTopics; t++) {
+          logLikelihoodOfDoc[i][t] = Math.log((currentDoc.topicsCounts[t] + beta) /
+                                             (bagOfWordsLength + numberOfTopics * beta))
+          meanLogLikelihood[t] += logLikelihoodOfDoc[i][t];
+
+        }
+      });
+
+      for (var t = 0; t < numberOfTopics; t++) {
+        meanLogLikelihood[t] /= numberOfDocuments;
+      }
+
+      for (var d = 0; d < numberOfDocuments; d++) {      
+        matrixEach(correlationMatrix, function(t1, t2){
+          correlationMatrix[t1][t2] +=  (logLikelihoodOfDoc[d][t1] - meanLogLikelihood[t1]) *
+                                        (logLikelihoodOfDoc[d][t2] - meanLogLikelihood[t2])
+        }, this);
+      }
+
+      console.log(meanLogLikelihood, correlationMatrix)
+      matrixEach(correlationMatrix, function(t1, t2) {
+        correlationMatrix[t1][t2] *= normalizer
       }, this);
-    }
 
-    console.log(meanLogLikelihood, correlationMatrix)
-    matrixEach(correlationMatrix, function(t1, t2) {
-      correlationMatrix[t1][t2] *= normalizer
-    }, this);
+      for (var t = 0; t < numberOfTopics; t++) {
+        standardDeviations[t] = Math.sqrt(correlationMatrix[t][t]);
+      }
 
-    for (var t = 0; t < numberOfTopics; t++) {
-      standardDeviations[t] = Math.sqrt(correlationMatrix[t][t]);
-    }
+      matrixEach(correlationMatrix, function(t1, t2){
+        correlationMatrix[t1][t2] /= (standardDeviations[t1] * standardDeviations[t2]);
+      }, this);
 
-    matrixEach(correlationMatrix, function(t1, t2){
-      correlationMatrix[t1][t2] /= (standardDeviations[t1] * standardDeviations[t2]);
-    }, this);
-
-    return correlationMatrix;
-  };
-
-  var initializeTopics = function(){
-    topics = [];
-    topicWeights = [];
-    for (var k = 0; k < numberOfTopics; k++) {
-      topicWeights[k] = (1/numberOfTopics);
-      topics[k] = {
-                  id:       k,
-                  withWord:  {}, 
-                  wordTotal: 0, 
-                  };
+      return correlationMatrix;
     };
-  for (w in words) {assignRandomly(words[w])};
+
+
+    this.getDocuments = function() {
+      return documents;
+    }
+
+    this.getTopics = function() {
+      return topics;
+    }
+
+    this.getVocabulary = function() {
+      return words;
+    }
+
+    initializeTopics();
   };
 
-  var assignRandomly = function(word) {
-    var random = Math.floor(Math.random() * numberOfTopics);
-    word.isTopic = random;
-    if (!topics[random].withWord[word.id]) topics[random].withWord[word.id] = 0;
-    topics[random].withWord[word.id]++;
-    topics[random].wordTotal++;
-  }
-
-  var recalculateBeta = function() {
-    alpha = numberOfWords/200;
-  }
-
-  var recalculateAlpha = function() {
-    beta = numberOfTopics/50
-  }
-
-  var nowCallback = function(callback, context, whatElse) {
-    if (!callback) return;
-    var dataObject = {topics: topics, 
-                      vocab: words,
-                      documents: documents
-                     };
-
-    callback.call(context, dataObject, whatElse)
-  }
-
-  // readParams(params);
-  initializeTopics();
-};
+  return Model;
+}());
